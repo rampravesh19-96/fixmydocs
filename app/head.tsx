00309d2835dba3d371cad989c38fbc36059db3dc
ram pravesh <rampraveshsingh1996@gmail.com>
2026-03-17 22:51:43 +0530

    Add image compression feature + SEO + favicon

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>FixMyDocs – Compress Images & Files Online for Free</title>
+      <meta name="description" content="FixMyDocs allows you to compress images and PDF files online for free. Fast, easy, and secure." />
+      <meta name="keywords" content="compress image online, compress pdf online, free image compressor, fix my docs" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
